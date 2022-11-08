d870e8dc386467cea89f79f1473a7bea4dd4f3f1
ru2saig <42857541+ru2saig@users.noreply.github.com>
2022-11-08 14:42:48 +0530

    Made the download multi-threaded to speed things up

--- main.py
+++ main.py
@@ -5,6 +5,46 @@ import random
 import numpy as np
 import pandas as pd
 from sklearn.decomposition import TruncatedSVD
+from concurrent.futures import ThreadPoolExecutor
+
+HEADERS = ({'User-Agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36', 'Accept-Language': 'en-US, en;q=0.5'})
+
+product_image_re = re.compile(r"((http|https)\:\/\/)?[a-zA-Z0-9\.\/\?\:@\-_=#]+\.([a-zA-Z]){2,6}([a-zA-Z0-9\.\&\/\?\:@\-_=#])*")
+product_title_re = re.compile("[|-]")
+
+
+def download_data(ASIN_id):
+    """
+    Download data from amazon.com by ASIN id.
+
+    Takes a single argument, the ASIN_id Does not use proxies,
+    or different ip addresses, or different headers. (yet)
+    could get ip banned if run too often
+    """
+    ASIN = ASIN_id
+    URL = f"https://www.amazon.com/exec/obidos/ASIN/{ASIN}"
+    webpage = requests.get(URL, headers=HEADERS)
+    if not webpage.ok:  # If they're not on amazon.com, they can be on other domains
+        print(f"Unable to find info on {ASIN_id} on amazon.com")
+        return ("404", "404")
+
+    print(f"Downloading Data of { ASIN_id }...")
+    soup = bs4.BeautifulSoup(webpage.content, "lxml")
+    title = soup.find("span", attrs={"id":'productTitle'})
+
+    product_name = product_title_re.split(title.string.strip())[0]
+    product_image = soup.find("div", attrs={"id" : "imgTagWrapperId"})
+    matches = product_image_re.finditer(str(product_image), re.MULTILINE)
+
+    for match in matches:
+        url = match.group()
+        if "http" in url:
+            product_image = url
+            break
+
+    print(f"Download for { ASIN_id } complete!")
+    return (product_name, product_image)
+
 
 amazon_ratings = pd.read_csv("customers_rating.csv")
 amazon_ratings_subset = amazon_ratings.head(20000)
@@ -18,7 +58,6 @@ decomposed_matrix = SVD.fit_transform(X)
 
 correlation_matrix = np.corrcoef(decomposed_matrix)
 
-HEADERS = ({'User-Agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36', 'Accept-Language': 'en-US, en;q=0.5'})
 
 while True:
     print("Product ID: Enter \"random\" for a random product")
@@ -38,35 +77,11 @@ while True:
     Recommend_list = list(X.index[correlation_product_ID > 0.90])
     Recommend_list.remove(product_id)
     Recommend_list.insert(0, product_id)
-    
+
     print(str(product_id).center(20, '='))
-    
-    for item in Recommend_list[:10]:
-        ASIN = item
-        URL = f"https://www.amazon.com/exec/obidos/ASIN/{ASIN}"
-        webpage = requests.get(URL, headers=HEADERS) # TODO: this takes around 3 seconds per request
 
-        if not webpage.ok: # If they're not on amazon.com, they can be on .uk or .ca, etc.
-            print(f"Unable to find info on {item} on amazon.com")
-            continue
+    with ThreadPoolExecutor() as executor:
+        results = executor.map(download_data, Recommend_list[:10])
 
-        print(f"Downloading Data of { item }...")
-
-        soup = bs4.BeautifulSoup(webpage.content, "lxml")
-        title = soup.find("span", attrs={"id":'productTitle'})
-
-        # compile the regex expressions, though
-        re.split("[|-]", title.string.strip()) # do smthing like [|//+] for multiple characters
-        product_name = re.split("[|]", title.string.strip())[0]
-
-        product_image = soup.find("div", attrs={"id" : "imgTagWrapperId"})
-        regex = r"((http|https)\:\/\/)?[a-zA-Z0-9\.\/\?\:@\-_=#]+\.([a-zA-Z]){2,6}([a-zA-Z0-9\.\&\/\?\:@\-_=#])*"
-        matches = re.finditer(regex, str(product_image), re.MULTILINE)
-
-        for match in matches:
-            url = match.group()
-            if "http" in url:
-                product_image = url
-                break
-
-        print(product_name, product_image)
+        for result in results:
+            print(result)
