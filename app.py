3c78db3f920b7577990bf7f51bd476b711587841
ru2saig <42857541+ru2saig@users.noreply.github.com>
2022-11-11 01:45:34 +0530

    Displays the searched and recommended results with images

--- app.py
+++ app.py
@@ -7,11 +7,71 @@ import pandas as pd
 from common import cache
 from fake_useragent import UserAgent, FakeUserAgentError
 from sklearn.decomposition import TruncatedSVD
-from concurrent.futures import ThreadPoolExecutor
 from flask import Flask, render_template, request
+from concurrent.futures import ThreadPoolExecutor
+
 
 app = Flask(__name__)
 cache.init_app(app=app, config={"CACHE_TYPE": "filesystem",'CACHE_DIR': '/tmp'})
+
+HEADERS = {'authority': 'www.amazon.com',
+            'pragma': 'no-cache',
+            'cache-control': 'no-cache',
+            'dnt': '1',
+            'upgrade-insecure-requests': '1',
+            'user-agent': 'Mozilla/5.0 (X11; CrOS x86_64 8172.45.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.64 Safari/537.36',
+            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+            'sec-fetch-site': 'none',
+            'sec-fetch-mode': 'navigate',
+            'sec-fetch-dest': 'document',
+            'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',}
+
+product_image_re = re.compile(r"((http|https)\:\/\/)?[a-zA-Z0-9\.\/\?\:@\-_=#]+\.([a-zA-Z]){2,6}([a-zA-Z0-9\.\&\/\?\:@\-_=#])*")
+product_title_re = re.compile("[|-]")
+
+
+def download_data(ASIN_id):
+    """
+    Download data from amazon.com by ASIN id.
+
+    Takes a single argument, the ASIN_id Does not use proxies,
+    or different ip addresses, or different headers. (yet)
+    could get ip banned if run too often
+    """
+    ASIN = ASIN_id
+    URL = f"https://www.amazon.com/exec/obidos/ASIN/{ASIN}"
+    try:
+        HEADERS['user-agent'] = UserAgent(cache=False).random
+    except FakeUserAgentError:
+        HEADERS['user-agent'] = 'Mozilla/5.0 (X11; CrOS x86_64 8172.45.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.64 Safari/537.36'
+
+    webpage = requests.get(URL, headers=HEADERS)
+
+    if not webpage.ok:  # If they're not on amazon.com, they can be on other domains
+        print(f"Unable to find info on {ASIN_id} on amazon.com")
+        return (404, 404)
+
+    if webpage.status_code > 500:
+        print("Request blocked by amazon. Try a proxy")
+        return ("Beep", "Boop")
+
+    print(f"Downloading Data of { ASIN_id }...")
+    soup = bs4.BeautifulSoup(webpage.content, "lxml")
+    title = soup.find("span", attrs={"id":'productTitle'})
+
+    product_name = product_title_re.split(title.string.strip())[0]
+    product_image = soup.find("div", attrs={"id" : "imgTagWrapperId"})
+    matches = product_image_re.finditer(str(product_image), re.MULTILINE)
+
+    for match in matches:
+        url = match.group()
+        if "http" in url:
+            product_image = url
+            break
+
+    print(f"Download for { ASIN_id } complete!")
+    return (product_name, product_image)
+
 
 @app.before_first_request
 def function_to_run_only_once(): # TODO: How to speed this up? This takes way too long!
@@ -36,7 +96,7 @@ def index():
     if request.method == 'POST':
         X = cache.get("X")
         correlation_matrix = cache.get("correlation_matrix")
-       
+
         product_id = request.form["productid"]
         if product_id == "random": # TODO make a button that does this
             product_id = X.index[random.randint(0, len(correlation_matrix))]
@@ -50,10 +110,20 @@ def index():
         Recommend_list.remove(product_id)
 
         print(str(product_id).center(20, "="))
-        print(Recommend_list[:10])
+        product_result = download_data(product_id)
+        
+        # TODO: Building a new dataset should fix this right up
+        results_data = []
+        with ThreadPoolExecutor() as executor:
+            results = executor.map(download_data, Recommend_list[:10])
 
-    
-    return render_template("index.html")
+            for result in results:
+                results_data.append(result)
+        print(results_data)
+
+        return render_template("index.html", product_result=product_result, results_data=results_data)
+
+    return render_template("index.html", product_result=("",""), results_data=[])
 
 
 if __name__ == "__main__":
